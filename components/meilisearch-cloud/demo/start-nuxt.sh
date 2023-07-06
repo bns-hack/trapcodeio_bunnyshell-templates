ee09a5fde091e1567591c831e41dc15da0fffd44
trapcodeio <trapcode.io@gmail.com>
2023-07-06 02:13:49 +0100

    add meilisearch demo

--- components/meilisearch-cloud/demo/start-nuxt.sh
+++ components/meilisearch-cloud/demo/start-nuxt.sh
@@ -4,11 +4,12 @@ sleep 5
 # Setup
 yarn run setup
 
-# Start
-# change env variables
+# change env variables because nuxt can't access `meilisearch:7700` host
 export MEILISEARCH_HOST=$MEILISEARCH_EXTERNAL_HOST
+
 # Build
 yarn run build
 
+# yarn run preview
 yarn run preview
 ```
