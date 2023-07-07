282dccee9c2730478dc0bac13d5668c31f8d93f1
trapcodeio <trapcode.io@gmail.com>
2023-07-07 05:26:43 +0100

    fix meilisearch

--- components/meilisearch-cloud/demo/start-nuxt-dev.sh
+++ components/meilisearch-cloud/demo/start-nuxt-dev.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # sleep for 5 seconds to wait for meilisearch to start
 sleep 5
 
@@ -7,9 +9,6 @@ yarn run setup
 # change env variables because nuxt can't access `meilisearch:7700` host
 export MEILISEARCH_HOST=$MEILISEARCH_EXTERNAL_HOST
 echo "Build with MEILISEARCH_HOST=$MEILISEARCH_HOST"
-# Build
-yarn run build
 
 # yarn run preview
-yarn run preview
-```
+yarn run dev
---
+++ components/meilisearch-cloud/demo/start-nuxt-prod.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# change env variables because nuxt can't access `meilisearch:7700` host
+# store current host in a local variable named `SEARCH_HOST`
+export SEARCH_HOST=$MEILISEARCH_HOST
+export MEILISEARCH_HOST=$MEILISEARCH_EXTERNAL_HOST
+echo "Build with MEILISEARCH_HOST=$MEILISEARCH_HOST"
+
+# Build
+yarn run build
+
+# Revert host changes
+export MEILISEARCH_HOST=$SEARCH_HOST
+echo "Run with MEILISEARCH_HOST=$MEILISEARCH_HOST"
+
+# Setup
+yarn run setup
+
+# yarn run preview
+yarn run preview
